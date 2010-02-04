e00dbc3b85929df735f634fe4c7d147393071d4a
Christoph Borgolte <c.borgolte@hudora.de>
2010-02-04 16:34:37 +0100

    Kommandozeilenprogramm zum manuellen Rückmelden von Kommibelegen
    siehe [#60]

---
+++ myplfrontend/wmslib.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+""" wmslib.py --- Funktionen rund um das Warehouse Management System
+"""
+
+# filename
+# Created by Christian Klein and Christoph Borgolte on 04-02-2010 for HUDORA.
+# Copyright (c) 2010 HUDORA. All rights reserved.
+
+
+import husoftm.lieferscheine
+import cs.messaging as messaging
+
+
+def kommibeleg_zurueckmelden(kommiauftragnr, zielqueue='erp.cs-wms.rueckmeldung#normal', audit_trail=''):
+    """Meldet einen Kommibeleg per Messaging zurück."""
+
+    chan = messaging.setup_queue(zielqueue, durable=True)
+    doc = messaging.empty_message('wmslib.rueckmelden/',
+                                  guid='mypl.kommiauftragsrueckmeldung-%s' % kommiauftragnr,
+                                  audit_trail=audit_trail,
+                                  audit_info='an SoftM zurueckgemeldet')
+    doc['kommiauftragnr'] = kommiauftragnr
+    doc['positionen'] = []
+    kommibeleg = husoftm.lieferscheine.Kommibeleg(kommiauftragnr)
+    for pos in kommibeleg.positionen:
+        doc['positionen'].append({'posnr': pos.kommissionierbeleg_position,
+                                  'menge': int(pos.menge_komissionierbeleg),
+                                  'artnr': pos.artnr})
+    messaging.publish(doc, zielqueue)
